3e4c03fb03c7ccc6c8787494648c38a6f8eb0a87
ElVicDev <vicesc820@gmail.com>
2025-04-18 00:37:03 -0600

    Hello World - Example

--- 100DaysOfCode/1_print.py
+++ 100DaysOfCode/1_print.py
@@ -3,11 +3,7 @@ print("Hello, World!") # This is a single line comment
 # los comentarios de una sola línea comienzan con hashtag (#)
 # y se extienden hasta el final de la línea.
 
-'''
-Los comentarios de varias líneas comienzan y terminan con tres comillas simples.
-Pueden abarcar varias líneas y son útiles para agregar descripciones más largas o explicaciones en el código.
-'''
 
-"""
-Los comentarios de varias líneas también pueden comenzar y terminar con tres comillas dobles.
-"""
+""" Los comentarios de varias líneas comienzan y terminan con tres comillas simples.
+También pueden comenzar y terminar con tres comillas dobles.
+Pueden abarcar varias líneas y son útiles para agregar descripciones más largas o explicaciones en el código. """
