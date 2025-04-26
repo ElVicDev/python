6a7e5d2a380325a6704f564f2b6e0125302f8033
ElVicDev <vicesc820@gmail.com>
2025-04-26 00:40:24 -0600

    For - Example 6

---
+++ py4e/1_basics/5_loops/for_6.py
@@ -0,0 +1,7 @@
+print('El siguiente programa suma los números de una lista dada')
+zork = 0
+print('Before', zork)
+for thing in [9, 41, 12, 3, 74, 15] :
+    zork = zork + thing
+    print(zork, thing)
+print('after', zork)
