84a8505b717609967e086f0a40d7350ff0064ca0
ElVicDev <vicesc820@gmail.com>
2025-04-25 20:39:45 -0600

    For - Example 2

---
+++ py4e/1_basics/5_loops/for_2.py
@@ -0,0 +1,5 @@
+print('El siguiente bucle le da Feliz Año a una lista de amigos')
+friends =['Joseph', 'Glenn', 'Sally']   # Cadena con lista de amigos
+for friend in friends :                 # iniciamos el bucle con for
+    print('Happy New Year: ', friend)   # Imprime una linea por cada amigo
+print('Done!')
