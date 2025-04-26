7d51994a65afd01f4f8238a6bf6b89af0f6213bf
ElVicDev <vicesc820@gmail.com>
2025-04-25 20:43:08 -0600

    For - Example 3

---
+++ py4e/1_basics/5_loops/for_3.py
@@ -0,0 +1,9 @@
+# Blucle que busca el número mayor
+print('El siguiente programa utiliza un bucle que busca el número mayor')
+largest_so_far = -1     # empezamos dando un numero negativo cualquiera
+print('Before', largest_so_far)
+for the_num in [9, 41,12,3,74,15] :
+    if the_num > largest_so_far :
+        largest_so_far = the_num
+    print(largest_so_far, the_num)
+print('After', largest_so_far)
