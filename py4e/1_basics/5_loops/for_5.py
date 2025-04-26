31a2e68dea7a9f89ad10e032fd273afbce717c66
ElVicDev <vicesc820@gmail.com>
2025-04-26 00:37:41 -0600

    For - Example 5

---
+++ py4e/1_basics/5_loops/for_5.py
@@ -0,0 +1,7 @@
+print('El siguiente programa enumero las iteraciones hechas con for')
+zork = 0
+print('Before', zork)
+for thing in [9, 41, 12, 3, 74, 15] :
+    zork = zork + 1
+    print(zork, thing)
+print('after', zork)
