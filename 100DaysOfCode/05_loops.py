55137b960534c951cd24eae68fd55dd448c3d89e
ElVicDev <vicesc820@gmail.com>
2025-07-26 00:31:54 -0600

    Conditional Statements and Loops

--- 100DaysOfCode/05_loops.py
+++ 100DaysOfCode/05_loops.py
@@ -15,3 +15,54 @@ While Loop
     Write a program to calculate the factorial of a given number.
 """
 # If-else Statements
+# Check if a number is even or odd
+number = int(input("Enter an integer: "))
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+
+# Determine age category
+age = int(input("\nEnter your age: "))
+if age < 13:
+    print("You are a child.")
+elif age < 20:
+    print("You are a teenager.")
+elif age < 60:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
+
+# Nested If-else Statements
+# Find the largest of three numbers
+num1 = int(input("\nEnter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+if num1 >= num2:
+    if num1 >= num3:
+        print(f"The largest number is {num1}.")
+    else:
+        print(f"The largest number is {num3}.")
+else:
+    if num2 >= num3:
+        print(f"The largest number is {num2}.")
+    else:
+        print(f"The largest number is {num3}.")
+
+# For Loop
+# Calculate the sum of all numbers up to a given number
+number = int(input("\nEnter a positive number to calculate the sum up to that number: "))
+sum = 0
+for n in range(1, number + 1):
+    sum += n
+print(f"The sum of all numbers up to {number} is {sum}.")
+
+# While Loop
+# Calculate the factorial of a given number
+number = int(input("\nEnter a positive integer to calculate its factorial: "))
+factorial = 1
+while number > 1:
+    factorial *= number
+    number -= 1
+print(f"The factorial of {number} is {factorial}.")
