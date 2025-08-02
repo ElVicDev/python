a52b4bc2ec0c5948a93fe0b7240a356dda3afe6f
ElVicDev <vicesc820@gmail.com>
2025-08-01 22:49:07 -0600

    While-loop

---
+++ 100DaysOfCode/11_whileLoop.py
@@ -0,0 +1,9 @@
+""" Día 11: mientras
+Use un bucle para imprimir números pares de 2 a 20.
+"""
+number = 0
+while number < 20:
+    number += 2
+    print(number)
+else:
+    print("Fin del bucle while.")
