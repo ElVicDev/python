f73ae753547d287e9b0b631d6cf82a685728a524
ElVicDev <vicesc820@gmail.com>
2025-08-02 22:48:10 -0600

    Odd-even

---
+++ 100DaysOfCode/12_oddEven.py
@@ -0,0 +1,8 @@
+""" Día 12: Odd-Even
+Escriba un programa para verificar si un número es uniforme o impar. """
+
+number = int(input("Ingrese un número: "))
+if number % 2 == 0:
+    print(f"{number} es un número par.")
+else:
+    print(f"{number} es un número impar.")
