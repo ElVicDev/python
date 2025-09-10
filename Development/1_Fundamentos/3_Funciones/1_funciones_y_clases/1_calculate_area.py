a8863d31291c88d49f85e6ee0ec1ff566d4501f9
ElVicDev <vicesc820@gmail.com>
2025-09-09 21:53:33 -0600

    Calcula el área de un rectángulo

---
+++ Development/1_Fundamentos/3_Funciones/1_funciones_y_clases/1_calculate_area.py
@@ -0,0 +1,11 @@
+""" Calcula el área de un rectángulo """
+
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+# Example usage:
+length = 5
+width = 3
+rectangle_area = calculate_area(length, width) # Function call, providing inputs
+print(f"The area of the rectangle is: {rectangle_area}") # Using the output
