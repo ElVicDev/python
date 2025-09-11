e97b0b5d561ad1f1282fab4a476977b02b7c133a
ElVicDev <vicesc820@gmail.com>
2025-09-10 22:22:34 -0600

    Calcular el promedio

---
+++ Development/1_Fundamentos/3_Funciones/2_organización/1_calculateAverage.py
@@ -0,0 +1,11 @@
+""" Calcular el promedio """
+
+def calculate_average(numbers):
+    total = 0  # Local variable
+    count = len(numbers)  # Local variable
+    for num in numbers:
+        total += num
+    average = total / count
+    return average
+
+print(calculate_average([2, 5, 4, 1]))
