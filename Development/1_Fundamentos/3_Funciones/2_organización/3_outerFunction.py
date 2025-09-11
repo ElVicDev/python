e9c75529e6c59a71354bd8798149919c041ccc44
ElVicDev <vicesc820@gmail.com>
2025-09-10 22:42:56 -0600

    Funciones externas - Funciones anidadas

---
+++ Development/1_Fundamentos/3_Funciones/2_organización/3_outerFunction.py
@@ -0,0 +1,11 @@
+""" Funciones externas - Funciones anidadas
+Una función puede estar definida dentro de otra función."""
+
+def outer_function():
+    outer_var = "Hello from the outer function!"
+    def inner_function():
+        print(outer_var)  # Accessible from the enclosing scope
+    inner_function()
+outer_function()
+# Two nested functions, the outer one labeled outer_function and 
+# the inner one inner_function
