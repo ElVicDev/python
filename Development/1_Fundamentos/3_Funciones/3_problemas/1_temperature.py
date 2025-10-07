4edefad0a70cd035b8d2a46bb9838881c2f5b81c
ElVicDev <vicesc820@gmail.com>
2025-10-07 01:40:09 -0600

    Temperature conversion

--- Development/1_Fundamentos/3_Funciones/3_problemas/1_temperature.py
+++ Development/1_Fundamentos/3_Funciones/3_problemas/1_temperature.py
@@ -12,10 +12,10 @@ def fahrenheit_to_celsius(fahrenheit):
 
 def convert_temperature(temperature, unit):
     """ Convierte la temperatura a la unidad especificada ('C' o 'F'). """
-    if unit.upper() == 'C':
-        return fahrenheit_to_celsius(temperature)
-    elif unit.upper() == 'F':
-        return celsius_to_fahrenheit(temperature)
+    if unit.upper() == 'F':
+        return celsius_to_fahrenheit(temperature)   # Convierte a Fahrenheit
+    elif unit.upper() == 'C':
+        return fahrenheit_to_celsius(temperature) # Convierte a Celsius
     else:
         raise ValueError("Unidad no válida. Usa 'C' para Celsius o 'F' para Fahrenheit.")
     
@@ -27,7 +27,12 @@ print(convert_temperature(212, 'F'))  # Debería devolver 413.6
 
 temperature_c = 25
 temperature_f = 77
+
+converted_f = convert_temperature(temperature_c, 'F') # Convierte 25°C a 77°F
+converted_c = convert_temperature(temperature_f, 'C') # Convierte 77°F a 25°C
+
 # Debería devolver 25°C is equal to: 77.0 °F
-print(f"{temperature_c}°C is equal to:", convert_temperature(temperature_c, 'F'), "°F")
+print(f"{temperature_c}°C is equal to {converted_f}°F")
+
 # Debería devolver 77°F is equal to: 25.0 °C
-print(f"{temperature_f}°F is equal to:", convert_temperature(temperature_f, 'C'), "°C")
+print(f"{temperature_f}°F is equal to {converted_c}°C")
