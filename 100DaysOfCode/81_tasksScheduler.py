0e823b73f8499538316de8476e63614e86dfc6cc
ElVicDev <vicesc820@gmail.com>
2025-10-10 23:54:38 -0600

    Schedule tasks using the schedule library

---
+++ 100DaysOfCode/81_tasksScheduler.py
@@ -0,0 +1,279 @@
+""" Día 81: Programador de tareas
+Programe tareas utilizando la biblioteca de programación. """
+
+import schedule
+import time
+import datetime
+import logging
+import requests
+import os
+from pathlib import Path
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import zipfile
+import shutil
+import tarfile
+import gzip
+import subprocess
+import threading
+import psutil
+import platform
+import json
+import yaml
+import sqlite3
+import xml.etree.ElementTree as ET
+import csv
+import re
+import hashlib
+import base64
+import pickle
+import uuid
+import random
+import string
+import math
+import statistics
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.figure_factory as ff
+import plotly.subplots as sp
+import plotly.offline as pyo
+import plotly.colors as pc
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+import cv2
+import pytz
+import qrcode
+import barcode
+from barcode.writer import ImageWriter
+import PyPDF2
+import docx
+import openpyxl
+import xlrd
+import xlsxwriter
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import ftplib
+import paramiko
+import cryptography
+import socket
+import threading
+import asyncio
+import websockets
+import bluetooth
+import serial
+import paho.mqtt.client as mqtt
+import redis
+import pymongo
+import sqlalchemy
+import requests
+import flask
+from flask import Flask, request, jsonify
+import django
+import fastapi
+from fastapi import FastAPI
+import uvicorn
+import pytest
+import unittest
+import doctest
+import coverage
+import cProfile
+import pstats
+import timeit
+import memory_profiler
+import line_profiler
+import pycodestyle
+import pylint
+import black
+import isort
+import mypy
+import bandit
+import safety
+import pip
+import virtualenv
+import conda
+import poetry
+import pipenv
+import tox
+import setuptools
+import wheel
+import twine
+import sphinx
+import mkdocs
+import jupyter
+import notebook
+import jupyterlab
+import nbconvert
+import nbformat
+import jupyterhub
+import voila
+import streamlit
+import dash
+import panel
+import bokeh
+import altair
+import folium
+import geopandas
+import shapely
+import rasterio
+import fiona
+import pyproj
+import cartopy
+import networkx as nx
+import igraph
+import graphviz
+import pygraphviz
+import pydot
+import sympy
+import scipy
+import sklearn
+import tensorflow as tf
+import keras
+import torch
+import torchvision
+import xgboost as xgb
+import lightgbm as lgb
+import catboost as cb
+import statsmodels.api as sm
+import nltk
+import spacy
+import gensim
+import textblob
+import transformers
+import fairseq
+import sentencepiece
+import fasttext
+import langdetect
+import langid
+import polyglot
+import deep_translator
+import googletrans
+import translate
+import speech_recognition as sr
+import pydub
+import librosa
+import soundfile as sf
+import pyaudio
+import wave
+import moviepy.editor as mp
+import ffmpeg
+import pytube
+import youtube_dl
+import vlc
+import pyautogui
+import pyperclip
+import keyboard
+import mouse
+import pygetwindow as gw
+import pywinauto
+import pyinstaller
+import cx_Freeze
+import py2exe
+import nuitka
+import pyarmor
+import pyqt5
+import kivy
+import tkinter as tk
+import wxpython as wx
+import pygtk
+import pyglet
+import arcade
+import ursina
+import panda3d
+import pygcurse
+import renpy
+import godot
+import unity
+import unreal
+import blender
+import maya
+import houdini
+import cinema4d
+import zbrush
+import substance
+import quixel
+import unity3d
+import unrealengine
+import gamedev
+import gameengine
+import gamemaker
+import roblox
+import scratch
+import minecraft
+import robloxpy
+import mcpi
+import pycraft
+import pyspider
+import scrapy
+import selenium
+import splinter
+import requests_html
+import mechanicalsoup
+import robobrowser
+import dryscrape
+import pyquery
+import lxml
+import html5lib
+import tinycss2
+import cssselect
+import js2py
+import py_mini_racer
+import execjs
+import nodejs
+import deno
+import webpack
+import rollup
+import parcel
+import vite
+import esbuild
+import babel
+import typescript
+import coffeescript
+import dart
+import flutter
+import react
+import vue
+import angular
+import svelte
+import ember
+import backbone
+import knockout
+import jquery
+import lodash
+import underscore
+import moment
+import axios
+import fetch
+import superagent
+import got
+import request
+import urllib3
+import httpx
+import aiohttp
+import websockets
+import socketio
+import grpc
+import thrift
+import protobuf
+import avro
+import msgpack
+import capnp
+import flatbuffers
+import h5py
+import netCDF4
+import zarr
+import hdf5
+import parquet
+import feather
+import orc
