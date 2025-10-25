eef4047ddb970897f80237ffb1261110f0408aa0
ElVicDev <vicesc820@gmail.com>
2025-10-25 14:30:38 -0600

    Data Analysys Module

---
+++ Development/1_Fundamentos/3_Funciones/4_modulos_y_bibliotecas/2_dataAnalysis/data_analysis_tools.py
@@ -0,0 +1,12 @@
+""" En este módulo, hemos definido una función llamada 
+    calculate_mean que aprovecha la biblioteca NumPy (np) 
+    para calcular la media de una lista de números. """
+
+import pandas as pd
+import numpy as np
+
+def calculate_mean(data):
+    """Calculates the mean of a list of numbers."""
+    return np.mean(data)
+
+# ... (other functions for median, mode, standard deviation)
