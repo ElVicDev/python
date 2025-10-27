9d5f6a2ccad0a1c873e71966d75dd113ae202549
ElVicDev <vicesc820@gmail.com>
2025-10-27 16:08:52 -0600

    Examples of lists and dictionaries

---
+++ Development/1_Fundamentos/4_Estructuras/1_Comprensión/1_shopping_lists.py
@@ -0,0 +1,25 @@
+""" Ejemplo prácticos en una aplicación de compras """
+
+shopping_list = ["apples", "bananas", "milk"]  # List for items
+item_quantities = {"apples": 3, "bananas": 1}  # Dictionary for quantities
+
+# User adds an item
+shopping_list.append("eggs")
+item_quantities["eggs"] = 12 
+
+# User increases the quantity of bananas
+item_quantities["bananas"] += 2
+
+# User removes apples
+shopping_list.remove("apples")
+del item_quantities["apples"]
+
+# Print updated list and dictionary
+print(shopping_list)
+print(item_quantities)
+
+""" Resultados esperados:
+El programa imprimirá la lista de compras actualizada y el diccionario de cantidades:
+['bananas', 'milk', 'eggs']
+{'bananas': 3, 'eggs': 12}
+"""
