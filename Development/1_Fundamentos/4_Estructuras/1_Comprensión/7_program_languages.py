d73f0a380f3a8ed078c877018fdf0990f2541aaa
ElVicDev <vicesc820@gmail.com>
2025-11-01 17:15:18 -0600

    SET - Example

---
+++ Development/1_Fundamentos/4_Estructuras/1_Comprensión/7_program_languages.py
@@ -0,0 +1,21 @@
+""" Crea un conjunto de lenguajes de programación. """
+
+# Create a set of favorite programming languages
+languages = {"Python", "JavaScript", "Java"}
+
+# Add "C++" to the set
+languages.add("C++")
+
+# Try to add "Python" again (it won't be added because it's a duplicate)
+languages.add("Python")
+print(languages)  # Output: {'Python', 'C++', 'JavaScript', 'Java'} (order may vary)
+
+# Remove "Java"
+languages.remove("Java")
+
+# Create another set of languages
+web_languages = {"JavaScript", "HTML", "CSS"}
+
+# Find common languages between the two sets
+common_languages = languages.intersection(web_languages)
+print(common_languages)  # Output: {'JavaScript'}
