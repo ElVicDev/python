482cc28127326328bab1d9e77cddf7ead8e5c68d
ElVicDev <vicesc820@gmail.com>
2025-11-10 23:40:12 -0600

    In Operator

---
+++ Development/1_Fundamentos/4_Estructuras/3_Ordenación_y_búsqueda/1_search/3_in_operator.py
@@ -0,0 +1,8 @@
+# Define a list of fruits
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+# Check if "banana" is in the list
+if "banana" in fruits:
+    print("Banana is in the list.")
+else:
+    print("Banana is not in the list.")
