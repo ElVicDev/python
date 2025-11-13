58c1720c9d7502e95715fa32fd472bf929fcb934
ElVicDev <vicesc820@gmail.com>
2025-11-12 23:46:21 -0600

    Index - Example

---
+++ Development/1_Fundamentos/4_Estructuras/3_Ordenación_y_búsqueda/1_search/5_index.py
@@ -0,0 +1,8 @@
+# Define a list of colors
+colors = ["red", "blue", "green", "yellow", "purple"]
+
+# Find the index of the frirst occurrence of "blue"
+first_blue_index = colors.index("blue")
+
+# Print the index
+print("The first occurrence of 'blue' is at index:", first_blue_index)
