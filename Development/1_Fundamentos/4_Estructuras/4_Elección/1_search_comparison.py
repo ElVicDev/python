95c18ea4597d63226c71c73960186b9815ce58f0
ElVicDev <vicesc820@gmail.com>
2025-11-14 00:10:01 -0600

    Comparison of searching in lists and dictionaries

---
+++ Development/1_Fundamentos/4_Estructuras/4_Elección/1_search_comparison.py
@@ -0,0 +1,18 @@
+""" Comparación de la búsqueda en listas y diccionarios """
+
+import timeit
+
+# List lookup
+list_data = list(range(100000))
+lookup_value = 99999
+list_time = timeit.timeit(lambda: lookup_value in list_data, number=1000)
+
+# Dictionary lookup
+dict_data = {i: i for i in range(100000)}
+dict_time = timeit.timeit(lambda: lookup_value in dict_data, number=1000)
+
+print("List lookup time:", list_time)
+print("Dictionary lookup time:", dict_time)
+
+""" La búsqueda por diccionario es significativamente más rápida que la búsqueda 
+    por lista, especialmente para grandes conjuntos de datos """
