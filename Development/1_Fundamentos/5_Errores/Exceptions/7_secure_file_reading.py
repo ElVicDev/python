3bd6fe18c3370278c8212e60fa16ac6687f1299b
ElVicDev <vicesc820@gmail.com>
2025-12-13 23:56:52 -0600

    Functional coding challenge: Safely reading files

---
+++ Development/1_Fundamentos/5_Errores/Exceptions/7_secure_file_reading.py
@@ -0,0 +1,18 @@
+"""     Escriba una función Python llamada read_file_contents que tome file_path como argumento.
+
+    Dentro de la función, utilice un bloque try-except para intentar abrir el archivo, leer su contenido e imprimirlo en la consola.
+
+    Maneje el FileNotFoundError específicamente imprimiendo un mensaje de error apropiado si el archivo no existe.
+
+Consejos:
+
+    Nombre su variable de archivo file.
+
+    Usa la sentencia with open(), que asegura que el archivo se cierra automáticamente, por lo que llamar a file.close() en el bloque finally no será necesario.
+
+Ejemplo Entrada:
+
+read_file_contents("/Users/Example/Documents/my_file.txt")
+Salida esperada:
+
+Error: Archivo no encontrado - /Usuarios/Ejemplo/Documentos/mi_archivo.txt """
